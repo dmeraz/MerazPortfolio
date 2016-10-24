23f0c0e673da52e2645be9e46a581841110b1d5d
dmeraz <darin.meraz@gmail.com>
2016-10-24 01:11:06 -0400

    added jquery.altportfolio.js and alt image ids to Porfolio items 1-3, js not working

---
+++ js/jquery.altportfolioimgs.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () { //anonymous function
+    $('code1').mouseover(400).fadeOut(function () {
+        $(this).attr('src', 'images/virtual-pet-code.png').fadeIn(500);
+    });
+});

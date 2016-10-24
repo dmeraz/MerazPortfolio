b0e6364e466fef6c547b3e2b85ad6f4b869caaca
dmeraz <darin.meraz@gmail.com>
2016-10-24 11:04:17 -0400

    styled Portfolio images to fit central div inline

--- js/jquery.altportfolioimgs.js
+++ js/jquery.altportfolioimgs.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () { //anonymous function
-    $('code1').mouseover(400).fadeOut(function () {
+    $("#code1").mouseover(400).fadeOut(function () {
         $(this).attr('src', 'images/virtual-pet-code.png').fadeIn(500);
     });
 });
